f7f510dbbb30d9e93533053fb0a2320060bd7d69
Jakob <Jakob.off@mail.thomasoff.de>
2023-09-05 20:37:18 +0200

    fixed boundary control

--- javascript/containerview.js
+++ javascript/containerview.js
@@ -1,8 +1,11 @@
+import { grid_size } from "./grid.js";
+
 class ContainerView
 {
-    constructor(pId, pTitle, pType, pX, pY,  pWidth, pHeight)
+    constructor(pId, pTitle, pType, pX, pY,  pWidth, pHeight, pContainer)
     {
-        this.id = pId; 
+        this.id = pId;
+        this.container = pContainer;
         this.position = {
             left: pX,
             top: pY,
@@ -11,8 +14,14 @@ class ContainerView
             width: pWidth,
             height: pHeight
         }
-        this.minWidth = 0;
-        this.minHeight = 0;
+        this.min  = {
+            width: 0,
+            height: 0,
+        }
+        this.max = {
+            x: -Math.max(),
+            y: -Math.max(),
+        }
         this.children = new Map();
         this.title = pTitle;
         this.type = pType;
@@ -21,21 +30,30 @@ class ContainerView
     }
     add(tblview)
     {
-        this.dimension.width = Math.max(this.dimension.width, tblview.position.left + tblview.dimension.width);
-        this.dimension.height = Math.max(this.dimension.height, tblview.position.top + tblview.dimension.height);
-        this.minHeight = this.dimension.height;
-        this.minWidth = this.dimension.width;
+        if(this.children.has(tblview.id)) return;
         this.children.set(tblview.id, tblview);
+        this.update_bounds();
+    }
+    remove(tblview)
+    {
+        if(!this.children.has(tblview.id)) return;
+        this.children.delete(tblview.id);
     }
 
-    move(x, y)
+    move(x, y, all = true)
     {
         let deltaX = x - this.position.left;
         let deltaY = y - this.position.top;
 
+        this.max.x += deltaX;
+        this.max.y += deltaY;
+        
         this.position.left = x;
         this.position.top = y;
 
+        if(this.container) this.container.update_bounds();
+
+        if(!all) return;
         for(let child of this.children.values())
         {   
             child.dragged = this.dragged;
@@ -43,16 +61,51 @@ class ContainerView
         }
 
     }
-    resize(width, height)
+
+
+    //This could cause performance issues
+    update_bounds()
     {
-        if(width > this.minWidth)
+        let children = Array.from(this.children.values());
+        if(children.length < 1) return;
+        children.sort((a,b) => {return a.position.left - b.position.left});
+        let most_left = children[0];
+        let most_right = children[children.length-1];
+        this.max.x = most_left.position.left - grid_size(0.5);
+        this.min.width = most_right.position.left + most_right.dimension.width - this.position.left + grid_size(0.5);
+        
+        children.sort((a,b) => {return a.position.top - b.position.top});
+        let most_top = children[0];
+        let most_bottom = children[children.length - 1];
+        this.max.y = most_top.position.top - grid_size(1);
+        this.min.height = most_bottom.position.top + most_bottom.dimension.height - this.position.top + grid_size(1);     
+    
+    }
+
+    resize(width, height, x, y)
+    {   
+        let upscale = x < this.position.left && width > this.dimension.width;
+        let in_bounds = x <= this.max.x && width >= this.min.width ;
+
+
+        if(upscale || in_bounds)
         {
+            this.position.left = x;
             this.dimension.width = width;
         }
-        if(height > this.minHeight)
+        
+        upscale = y < this.position.top && height > this.dimension.height;
+        in_bounds = y <= this.max.y && height >= this.min.height ;
+
+        if(upscale || in_bounds)
         {
+            this.position.top = y;
             this.dimension.height = height;
-        }    
+        } 
+        else {
+            //this.position.top = this.max.y;
+        }
+        this.update_bounds();
     }
 }
 
