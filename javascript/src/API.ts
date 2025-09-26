1b4cfeff3f6ab4bc1d8a13b5acbc3dfb4b0fc210
Jakob <Jakob.off@mail.thomasoff.de>
2025-09-26 14:24:01 +0200

    fixed issue #17 API.select_element('', false) bringt noch immer Fehler wegen unbekannter ID

--- javascript/src/API.ts
+++ javascript/src/API.ts
@@ -148,8 +148,7 @@ try {
 export function select_element(id:string, bool = true)
 {
 try {
-    let element = _try_get<View>(id);
-    if(!element)
+    if(id == "" || !_try_get<View>(id))
     {
         diagview.select_multiple(Array.from(diagview.elements.keys()), bool);
     }
